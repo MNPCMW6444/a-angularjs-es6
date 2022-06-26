47cf9d3aa6ab334428da2bf6f11d25c44d879122
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-06-26 14:08:29 +0300

    asd

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 (function (angular) {
   "use strict";
-  var app = angular.module("form-example1", []);
-
-  app.directive("integer", function () {
-    return {
-      require: "ngModel",
-      link: function (scope, elm, attrs, ctrl) {
-        ctrl.$validators.integer = function (modelValue, viewValue) {
-          if (ctrl.$isEmpty(modelValue)) {
-            return true;
-          }
-          if (
-            !viewValue ||
-            viewValue === "-" ||
-            (new RegExp(attrs.allowedChars).test(viewValue) &&
-              parseFloat(viewValue) >= parseFloat(attrs.minVal) &&
-              parseFloat(viewValue) <= parseFloat(attrs.maxVal) &&
-              (!viewValue.includes(".") ||
-                viewValue.split(".")[1].length <= attrs.toFixed))
-          ) {
-            return true;
-          }
-
-          return false;
-        };
-      },
-    };
-  });
+  angular
+    .module("myApp", [])
+    .controller("MyTrainingController", function ($scope) {
+      $scope.myModalVariable = 10;
+    })
+    .filter("convertToMeters", function () {
+      var conversionKey = 0.3048;
+      return function (myModalVariable) {
+        return myModalVariable * conversionKey;
+      };
+    })
+    .directive("integer", function () {
+      return {
+        require: "ngModel",
+        link: function (scope, elm, attrs, ctrl) {
+          ctrl.$validators.integer = function (modelValue, viewValue) {
+            if (ctrl.$isEmpty(modelValue)) {
+              return true;
+            }
+            if (
+              !viewValue ||
+              viewValue === "-" ||
+              (new RegExp(attrs.allowedChars).test(viewValue) &&
+                parseFloat(viewValue) >= parseFloat(attrs.minVal) &&
+                parseFloat(viewValue) <= parseFloat(attrs.maxVal) &&
+                (!viewValue.includes(".") ||
+                  viewValue.split(".")[1].length <= attrs.toFixed))
+            ) {
+              return true;
+            }
+            return false;
+          };
+        },
+      };
+    });
 })(window.angular);
