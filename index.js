c893bbe5ed5c829b9c410d67cb7b5f67baaa4d91
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-06-27 10:36:48 +0300

    Part 1 complete

--- index.js
+++ index.js
@@ -1,38 +1,87 @@
-(function (angular) {
-  "use strict";
-  angular
-    .module("myApp", [])
-    .controller("MyTrainingController", function ($scope) {
-      $scope.myModalVariable = 10;
-    })
-    .filter("convertToMeters", function () {
-      var conversionKey = 0.3048;
-      return function (myModalVariable) {
-        return myModalVariable * conversionKey;
+let app = angular.module("myApp", []);
+app.constant("CONVERSION_KEY", 3.24);
+app.controller("MyTrainingController", function ($scope) {
+  $scope.myModelVariable = 10;
+});
+app.directive("minVal", function () {
+  return {
+    require: "ngModel",
+    link: function (scope, elm, attrs, ctrl) {
+      ctrl.$validators.minVal = function (modelValue, viewValue) {
+        if (
+          !viewValue ||
+          viewValue === "-" ||
+          parseFloat(viewValue) >= parseFloat(attrs.minVal)
+        )
+          return true;
+        return false;
       };
-    })
-    .directive("integer", function () {
-      return {
-        require: "ngModel",
-        link: function (scope, elm, attrs, ctrl) {
-          ctrl.$validators.integer = function (modelValue, viewValue) {
-            if (ctrl.$isEmpty(modelValue)) {
-              return true;
-            }
-            if (
-              !viewValue ||
-              viewValue === "-" ||
-              (new RegExp(attrs.allowedChars).test(viewValue) &&
-                parseFloat(viewValue) >= parseFloat(attrs.minVal) &&
-                parseFloat(viewValue) <= parseFloat(attrs.maxVal) &&
-                (!viewValue.includes(".") ||
-                  viewValue.split(".")[1].length <= attrs.toFixed))
-            ) {
-              return true;
-            }
-            return false;
-          };
-        },
+    },
+  };
+});
+app.directive("maxVal", function () {
+  return {
+    require: "ngModel",
+    link: function (scope, elm, attrs, ctrl) {
+      ctrl.$validators.maxVal = function (modelValue, viewValue) {
+        if (
+          !viewValue ||
+          viewValue === "-" ||
+          parseFloat(viewValue) <= parseFloat(attrs.maxVal)
+        ) {
+          elm["0"].style.color = "black";
+          return true;
+        }
+        elm["0"].style.color = "red";
+        return false;
       };
-    });
-})(window.angular);
+    },
+  };
+});
+app.directive("allowedChars", function () {
+  return {
+    require: "ngModel",
+    link: function (scope, elm, attrs, ctrl) {
+      ctrl.$validators.allowedChars = function (modelValue, viewValue) {
+        if (
+          !viewValue ||
+          viewValue === "-" ||
+          new RegExp(attrs.allowedChars).test(viewValue)
+        ) {
+          elm["0"].style.color = "black";
+          return true;
+        }
+        elm["0"].style.color = "red";
+        return false;
+      };
+    },
+  };
+});
+app.directive("displayFilter", function (CONVERSION_KEY) {
+  return {
+    require: "ngModel",
+    link: function (scope, elm, attrs, ctrl) {
+      function parseViewValue(modelValue) {
+        return Math.round((modelValue / CONVERSION_KEY) * 100) / 100;
+      }
+      ctrl.$parsers.push(parseViewValue);
+    },
+  };
+});
+app.directive("toFixed", function () {
+  return {
+    require: "ngModel",
+    link: function (scope, elm, attrs, ctrl) {
+      ctrl.$validators.toFixed = function (modelValue, viewValue) {
+        if (
+          !viewValue ||
+          viewValue === "-" ||
+          !viewValue.includes(".") ||
+          viewValue.split(".")[1].length <= attrs.toFixed
+        )
+          return true;
+        return false;
+      };
+    },
+  };
+});
